da9765960d19fb5ed233fb22262d101578f51e6a
Fumegalli <rafaelxxc@gmail.com>
2019-09-18 11:22:19 -0300

    Formatting code

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,11 @@
-import {
-  Router
-} from 'express';
+import { Router } from 'express'
 
-const routes = new Router();
+const routes = new Router()
 
-routes.get('/', (req, res) => {
-  return res.json({
-    message: 'Hello Mundo'
+routes.get('/', (req, res) =>
+  res.json({
+    message: 'Hello Mundo',
   })
-});
+)
 
-export default routes;
+export default routes
