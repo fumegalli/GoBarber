791084a34aa2907fd7da79f445df537e72d67a11
Fumegalli <rafaelxxc@gmail.com>
2019-09-18 16:00:23 -0300

    Add session

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '6af138e20c6c6b168cc20c9521c76699',
+  expiresIn: '7d',
+}
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express'
 
 import UserController from './app/controllers/UserController'
+import SessionController from './app/controllers/SessionController'
 
 const routes = new Router()
 
 routes.post('/users', UserController.create)
+routes.post('/session', SessionController.create)
+
+routes.put('/users', UserController.update)
 
 export default routes
