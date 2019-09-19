48d45bde1d6f020b238cec5dbdf2ecd2a84ec955
Fumegalli <rafaelxxc@gmail.com>
2019-09-19 16:49:50 -0300

    Add postfile route

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express'
+import multer from 'multer'
+import multerConfig from './config/multer'
 
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
+import FileController from './app/controllers/FileController'
 
 import authMiddleware from './app/middlewares/auth'
 
 const routes = new Router()
+const upload = multer(multerConfig)
 
 routes.post('/users', UserController.create)
 routes.post('/session', SessionController.create)
@@ -13,5 +17,7 @@ routes.post('/session', SessionController.create)
 routes.use(authMiddleware)
 
 routes.put('/users', UserController.update)
+
+routes.post('/files', upload.single('file'), FileController.create)
 
 export default routes
