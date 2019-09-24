b14f2fc6443674e8eb8cdebff7381ae42f449ee4
Fumegalli <rafaelxxc@gmail.com>
2019-09-24 14:35:34 -0300

    Add mail config

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '49cc0627ca4f2f',
+    pass: 'f128e580609bbe',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+}
