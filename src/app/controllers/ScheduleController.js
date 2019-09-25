a16c6caa0f8a01a1bb3716ab85e3e873c1054daa
Fumegalli <rafaelxxc@gmail.com>
2019-09-25 10:52:15 -0300

    Fix schedule get method

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -1,3 +1,6 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns'
+import { Op } from 'sequelize'
+
 import Appointment from '../models/Appointment'
 import User from '../models/User'
 
@@ -17,13 +20,20 @@ class ScheduleController {
     }
 
     const { date } = req.query
+    const parsedDate = parseISO(date)
 
-    // TODO: Ver essa classe do Diego e ver como está.
-    // const appointments = await appointments.findAll({
-    //   where:
-    // })
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+      order: ['date'],
+    })
 
-    return res.json(date)
+    return res.status(200).json(appointments)
   }
 }
 
